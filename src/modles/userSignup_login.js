ddaa7526ffb010c0f4b8a5198a737fa14f74893c
zeeshan0987 <zeeshan0987ali@gmail.com>
2023-12-24 18:50:59 +0530

    update

--- src/modles/userSignup_login.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  // roleId: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
-  FirstName: String,
-  Mobile: String,
-});
-
-export default mongoose.model("UserSignup_login", userSchema);
